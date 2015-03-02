561aad246ecb565ed61e6aef280fd80ca9a09bc9
pchtsp <pchtsp@gmail.com>
2015-03-02 20:57:17 +0100

    lesson2 complete

--- webserver.py
+++ webserver.py
@@ -1,5 +1,23 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+import re
+
+engine= create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind= engine
+
+DBSession= sessionmaker(bind= engine)
+session= DBSession()
+
+def getIDfrompath(path):
+	p=re.compile('\d+')
+	p_answer= p.search(path)
+	if p_answer==None:
+		return False
+	else: 
+		return int(p.search(path).group())
 
 class webserverHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
@@ -15,45 +33,149 @@ class webserverHandler(BaseHTTPRequestHandler):
 				output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
 				output+= "</body></html>"
 				self.wfile.write(output)
-				print output
+				# print output
 				return
 
-			if self.path.endswith("/hola"):
+			if self.path.endswith("/restaurants"):
 				self.send_response(200)
-				self.send_header('Conten-type','text/html')
+				self.send_header('content-type','text/html')
 				self.end_headers()
 
 				output= ""
 				output+= "<html><body>"
-				output+= "&#161Buenas! <a href= '/hello'>Back to Hello"
-				output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+				allRestaurants= session.query(Restaurant).all()
+
+				#we print all the records of restaurants names':
+				for resto in allRestaurants:
+					output+= "<p>"
+					output+= "<h2>"+resto.name+ "</h2>"
+					output+= "<a href='restaurants/"+str(resto.id)+"/edit'>Edit</a><br/>"
+					output+= "<a href='restaurants/"+str(resto.id)+"/delete'>Delete</a>"
+					output+= "<br>"
+					output+= "</p>"
+
+				output+= "<a href='/restaurants/new'>Create a new Restaurant</a><br/>"
+				output+= "</body></html>"
+
+				self.wfile.write(output)
+				#print output
+				return
+
+			if self.path.endswith("/restaurants/new"):
+				self.send_response(200)
+				self.send_header('content-type','text/html')
+				self.end_headers()
+
+				output= ""
+				output+= "<html><body>"
+				output+= "new Resto insert page..."
+				output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/new'><h2>What's the name of your new restaurant?</h2><input name="newResto" type="text" ><input type="submit" value="Submit"> </form>'''
 				output+= "</body></html>"
 				self.wfile.write(output)
-				print output
+				# print output
 				return
+			
+			#if we find a number in the path: maybe it is an id for a restaurant:
+			if getIDfrompath(self.path):
+				#we initialize output
+				output=""
+				#we need to know if the restaurant really exists:
+				allRestaurants= session.query(Restaurant).all()
+				id_list= [resto.id for resto in allRestaurants]
+				possible_id= getIDfrompath(self.path)
+				#print possible_id
+				#print id_list
+				if possible_id not in id_list: 
+					self.send_error(404,"File not found %s" % self.path)
+					return
+				#since the id exists: we will take the resto that corresponds to that id:
+				restoToEdit= session.query(Restaurant).filter_by(id=possible_id).one()
+				print "creo objeto"
+				#now: we need to know if we will edit it or delete it:
+				if self.path.endswith("/edit"):
+					print "llego al /edit"
+					self.send_response(200)
+					self.send_header('content-type','text/html')
+					self.end_headers()
+					output+= "<html><body>"
+					output += "<form method='POST' enctype='multipart/form-data' action='"+self.path+"'><h2>"+restoToEdit.name+"</h2><h3>Please, insert the new name for the restaurant:</h3><input name='editResto' type='text' ><input type='submit' value='Submit'> </form>"
+					output+= "</body></html>"
+					self.wfile.write(output)
+					return
+
+				if self.path.endswith("/delete"):
+					self.send_response(200)
+					self.send_header('content-type','text/html')
+					self.end_headers()
+					output+= "<html><body>"
+					output += "<form method='POST' enctype='multipart/form-data' action='"+self.path+"'><h2>Are you sure you want to delete restaurant named '"+restoToEdit.name +"'?</h2><input type='submit' value='Yes'> </form>"
+					output+= "</body></html>"
+					self.wfile.write(output)
+					return
+
 
 		except IOError:
 			self.send_error(404,"File not found %s" % self.path)
 
 	def do_POST(self):
+		print "enter POST"
 #		try:
 		self.send_response(301)
 		self.end_headers()
 
-		ctype, pdict= cgi.parse_header(self.headers.getheader('content-type'))
-		if ctype== 'multipart/form-data':
-			fields= cgi.parse_multipart(self.rfile, pdict)
-			messagecontent= fields.get('message')
-
+		messagecontent=[]
+		print self.path
 		output= ""
-		output+= "<html><body>"
-		output+= "<h2> Okay, how about this: <h2>"
-		output+= "<h1> %s </h1>" % messagecontent[0]
 
-		output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-		output+= "</body></html>"
-		self.wfile.write(output)
-		print output
+		#here we create a new restaurant.
+		if self.path.endswith("/restaurants/new"):
+			ctype, pdict= cgi.parse_header(self.headers.getheader('content-type'))
+			if ctype== 'multipart/form-data':
+				fields= cgi.parse_multipart(self.rfile, pdict)
+				messagecontent= fields.get('newResto')
+			if len(messagecontent)>0:
+				new_restaurant= Restaurant(name= messagecontent[0])
+				session.add(new_restaurant)
+				session.commit()
+				output= "<html><body><h1> Restaurante creado! </h1> <a href='/restaurants'>Volver a restaurantes</a> </body></html>"
+			else:
+				output= "<html><body><h1> Hubo algun problema... </h1> <a href='/restaurants'>Volver a restaurantes</a> </body></html>"
+			self.wfile.write(output)
+			print output
+			return
+
+		possible_id= getIDfrompath(self.path)
+		if not possible_id: 
+			self.send_error(404,"File not found %s" % self.path)
+			return
+		allRestaurants= session.query(Restaurant).all()
+		id_list= [resto.id for resto in allRestaurants]
+		if possible_id not in id_list: 
+			self.send_error(404,"File not found %s" % self.path)
+			return
+		restoToEdit= session.query(Restaurant).filter_by(id=possible_id).one()
+		#here we delete an existing restaurant
+		if self.path.endswith("/delete"):
+			#since the id exists: we will take the resto that corresponds to that id:
+			session.delete(restoToEdit)
+			session.commit()
+			output= "<html><body><h1> Restaurante eliminado! </h1> <a href='/restaurants'>Volver a restaurantes</a> </body></html>"
+			self.wfile.write(output)
+
+		if self.path.endswith("/edit"):
+			#since the id exists: we will take the resto that corresponds to that id:
+			ctype, pdict= cgi.parse_header(self.headers.getheader('content-type'))
+			if ctype== 'multipart/form-data':
+				fields= cgi.parse_multipart(self.rfile, pdict)
+				messagecontent= fields.get('editResto')
+			if len(messagecontent)>0:
+				restoToEdit.name=messagecontent[0]
+				session.add(restoToEdit)
+				session.commit()
+				output= "<html><body><h1> Restaurante editado! </h1> <a href='/restaurants'>Volver a restaurantes</a> </body></html>"
+			else:
+				output= "<html><body><h1> Hubo algun problema... </h1> <a href='/restaurants'>Volver a restaurantes</a> </body></html>"
+			self.wfile.write(output)
 
 #		except:
 #			pass
